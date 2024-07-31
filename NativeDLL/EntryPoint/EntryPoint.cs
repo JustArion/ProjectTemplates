f0f452121b66a61e0c320e8678503862a152ab55
arion <53127594+JustArion@users.noreply.github.com>
2024-07-31 17:43:42 +0200

    Use ModuleInitializer and GetModuleHandle to mimic a DLLMain

--- NativeDLL/EntryPoint/EntryPoint.cs
+++ NativeDLL/EntryPoint/EntryPoint.cs
@@ -2,34 +2,35 @@
 
 internal static class EntryPoint
 {
-    internal static HINSTANCE HModule { get; private set; }
+    private static bool _initialized;
+    
+    [ModuleInitializer]
+    [Obsolete("This function can only be called by the runtime!", true)]
+    internal static void Initialize()
+    {
+        HModule = GetModuleHandle();
+        DisableThreadLibraryCalls(HModule);
+        CreateThread(null, default, MainThread, nint.Zero, CREATE_THREAD_FLAGS.RUN_IMMEDIATELY, out _);
+    }
+
+    internal static SafeHINSTANCE HModule { get; private set; } = null!;
     /// <summary>
     /// https://learn.microsoft.com/en-us/windows/win32/dlls/dllmain
     /// </summary>
     [UnmanagedCallersOnly(EntryPoint = nameof(DllMain), CallConvs = [typeof(CallConvStdcall)])]
-    internal static bool DllMain(HINSTANCE module, NativeDLLCallReason fdwReason, nint lpReserved)
-    {
-        if (fdwReason != DLL_PROCESS_ATTACH) 
-            return true;
-        
-        DisableThreadLibraryCalls(module);
-        CreateThread(null, default, MainThread, (nint)module, CREATE_THREAD_FLAGS.RUN_IMMEDIATELY, out _);
-
-        return true;
-    }
+    internal static bool DllMain(HINSTANCE module, NativeDLLCallReason fdwReason, nint lpReserved) => true;
     
-    private static uint MainThread(nint hModule)
+    private static uint MainThread(nint _)
     {
         try
         {
-            HModule = hModule;
-            Start.DllMain();
+            Task.Run(Start.DllMain);
         }
         catch (Exception e)
         {
             Console.Error.WriteLine(e);
         }
-        
-        return 1;
+
+        return 0;
     }
 }
